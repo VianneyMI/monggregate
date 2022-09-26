60da1b2689b61b0c066af8a92ba914238275cda7
VianneyMI <vianney.mixtur@outlook.fr>
2022-09-26 10:15:08 +0200

    packaging

--- monggregate/__init__.py
+++ monggregate/__init__.py
@@ -1,3 +1,32 @@
 """App Package"""
 
-from . import expressions
+from monggregate.pipeline import Pipeline
+from monggregate.stages import(
+    AddFields,
+    BucketAuto,
+    Bucket,
+    Count,
+    Explode,
+    Group,
+    Limit,
+    Lookup,
+    Match,
+    Out,
+    Project,
+    ReplaceWith,
+    ReplaceRoot,
+    Sample,
+    Set,
+    Skip,
+    SortByCount,
+    Sort,
+    Stage,
+    Unwind
+)
+from monggregate import expressions
+
+__version__ = "0.2.0"
+__author__ = "Vianney Mixtur"
+__contact__ = "vianney.mixtur@outlook.fr"
+__copyright__ = "TBD" # TODO : Explore this <VM, 26/09/2022>
+__license__ = "MIT"
