7ea8d518b59066ca4589461fd14301bffbfd50b6
VianneyMI <vianney.mixtur@outlook.fr>
2022-12-29 14:32:45 +0100

    Debugging bumpver

--- monggregate/__init__.py
+++ monggregate/__init__.py
@@ -25,7 +25,8 @@ from monggregate.stages import(
 )
 from monggregate import expressions
 
-__version__ = "0.7.0" # IMPORTANT : Please think about updated the pyproject.toml file when changing the version here.
+# IMPORTANT : Please think about updated the pyproject.toml file when changing the version here.
+__version__ = "0.7.0"
 __author__ = "Vianney Mixtur"
 __contact__ = "vianney.mixtur@outlook.fr"
 __copyright__ = "TBD" # TODO : Explore this <VM, 26/09/2022>
