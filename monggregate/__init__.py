bb0ec75745e59ea729f04344b8e4a84b0ac721b4
VianneyMI <vianney.mixtur@outlook.fr>
2022-12-29 14:42:49 +0100

    bump version 0.7.1 -> 0.8.0

--- monggregate/__init__.py
+++ monggregate/__init__.py
@@ -26,7 +26,7 @@ from monggregate.stages import(
 from monggregate import expressions
 
 # IMPORTANT : Please think about updated the pyproject.toml file when changing the version here.
-__version__ = "0.7.1"
+__version__ = "0.8.0"
 __author__ = "Vianney Mixtur"
 __contact__ = "vianney.mixtur@outlook.fr"
 __copyright__ = "TBD" # TODO : Explore this <VM, 26/09/2022>
