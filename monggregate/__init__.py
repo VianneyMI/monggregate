270bdf395985ff11158a29793f9b95f8e6152ae8
VianneyMI <vianney.mixtur@outlook.fr>
2022-12-29 16:05:52 +0100

    Added copyright information

--- monggregate/__init__.py
+++ monggregate/__init__.py
@@ -1,42 +1,11 @@
 """App Package"""
 
 from monggregate.pipeline import Pipeline
-from monggregate.stages import(
-    Stage,
-    AddFields,
-    BucketAuto,
-    Bucket,
-    Count,
-    Explode,
-    Group,
-    Limit,
-    Lookup,
-    Match,
-    Out,
-    Project,
-    ReplaceWith,
-    ReplaceRoot,
-    Sample,
-    Set,
-    Skip,
-    SortByCount,
-    Sort,
-    Unwind
-)
-from monggregate import expressions
+from monggregate.expressions.expressions import Expression
 
 # IMPORTANT : Please think about updated the pyproject.toml file when changing the version here.
 __version__ = "0.8.0"
 __author__ = "Vianney Mixtur"
 __contact__ = "vianney.mixtur@outlook.fr"
-__copyright__ = "TBD" # TODO : Explore this <VM, 26/09/2022>
+__copyright__ = "Copyright © 2022 Vianney Mixtur"
 __license__ = "MIT"
-
-# Standard Library imports
-#----------------------------
-
-# 3rd Party imports
-# ---------------------------
-
-# Package imports
-# ---------------------------
