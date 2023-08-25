76529bf7b0e3de46a93fdbbbe3ea500205cecfcc
Vianney Mixtur <vianney.mixtur@outlook.fr>
2023-08-25 17:31:13 +0200

    Integrated new operators into expressions module

--- monggregate/expressions.py
+++ monggregate/expressions.py
@@ -1,11 +1,5 @@
 """Expressions Module"""
 
-# https://github.com/pydantic/pydantic/issues/2279
-# https://stackoverflow.com/questions/53638973/recursive-type-annotations
-# https://stackoverflow.com/questions/53845024/defining-a-recursive-type-hint-in-python
-
-# TO HELP REFACTOR: https://www.practical-mongodb-aggregations.com/guides/expressions.html
-
 # Standard Library imports
 #----------------------------
 from typing import Any, Literal
@@ -30,10 +24,6 @@ from monggregate.operators import(
     strings,  
     type_,
 )
-#Expressions can include field paths, literals, system variables, expression objects, and expression operators. Expressions can be nested.
-#ExpressionObjects {field1: Expression}
-#OperatorExpression {operator:[arg, .. argN]} or {operator:arg}
-
 
 class Expression(BaseModel):
     """
@@ -79,9 +69,240 @@ class Expression(BaseModel):
 
         return cls(content=Variable(name))
     
-
     #---------------------------------------------------
-    # Logical Operators
+    # Accumulators (Aggregation) Operators
+    #---------------------------------------------------
+    def average(self)->Self:
+        """Creates an $avg expression"""
+
+       
+        return self.__class__(content=accumulators.Avg(expression=self))
+        
+
+    def count(self)->Self:
+        """Creates a $count expression"""
+
+       
+        return self.__class__(content=accumulators.Count())
+        
+
+    def first(self)->Self:
+        """Creates a $first expression"""
+
+       
+        return  self.__class__(content=accumulators.First(expression=self))
+        
+
+    def last(self)->Self:
+        """Creates a $last expression"""
+
+       
+        return self.__class__(content=accumulators.Last(expression=self))
+        
+
+    def max(self)->Self:
+        """Creates a $max expression"""
+
+       
+        return self.__class__(content=accumulators.Max(expression=self))
+        
+
+    def min(self)->Self:
+        """Creates a $min expression"""
+
+       
+        return self.__class__(content=accumulators.Min(expression=self))
+        
+
+    def push(self)->Self:
+        """Creates a $push expression"""
+
+       
+        return  self.__class__(content=accumulators.Push(expression=self))
+        
+
+    def sum(self)->Self:
+        """Creates a $sum expression"""
+
+        return  self.__class__(content=accumulators.Sum(expression=self))
+        
+
+    
+    #---------------------------------------------------
+    # Arithmetic Operators
+    #---------------------------------------------------
+    def __add__(self, other:Self)->Self:
+        """
+        Creates a $add expression.
+
+        Overloads python addition operator (+).
+        """
+
+       
+        return self.__class__(content=arithmetic.Add(left=self, right=other))
+    
+
+    def __sub__(self, other:Self)->Self:
+        """
+        Creates a $subtract expression.
+
+        Overloads python subtraction operator (-).
+        """
+
+       
+        return self.__class__(content=arithmetic.Subtract(left=self, right=other))
+    
+
+    def __mul__(self, other:Self)->Self:
+        """
+        Creates a $multiply expression.
+
+        Overloads python multiplication operator (*).
+        """
+
+       
+        return self.__class__(content=arithmetic.Multiply(left=self, right=other))
+    
+
+    def __div__(self, other:Self)->Self:
+        """
+        Creates a $divide expression.
+
+        Overloads python division operator (/).
+        """
+
+       
+        return self.__class__(content=arithmetic.Divide(left=self, right=other))
+    
+
+    def __pow__(self, other:Self)->Self:
+        """
+        Creates a $pow expression.
+
+        Overloads python power operator (**).
+        """
+
+       
+        return self.__class__(content=arithmetic.Pow(left=self, right=other))
+    
+
+    def __radd__(self, other:Self)->Self:
+        """
+        Creates a $add expression.
+
+        Overloads python addition operator (+).
+        """
+
+       
+        return self.__class__(content=arithmetic.Add(left=other, right=self))
+    
+
+    def __rsub__(self, other:Self)->Self:
+        """
+        Creates a $subtract expression.
+
+        Overloads python subtraction operator (-).
+        """
+
+       
+        return self.__class__(content=arithmetic.Subtract(left=other, right=self))
+    
+
+    def __rmul__(self, other:Self)->Self:
+        """
+        Creates a $multiply expression.
+
+        Overloads python multiplication operator (*).
+        """
+
+       
+        return self.__class__(content=arithmetic.Multiply(left=other, right=self))
+    
+    def __rdiv__(self, other:Self)->Self:
+        """
+        Creates a $divide expression.
+
+        Overloads python division operator (/).
+        """
+
+       
+        return self.__class__(content=arithmetic.Divide(left=other, right=self))
+    
+
+    def __rpow__(self, other:Self)->Self:
+        """
+        Creates a $pow expression.
+
+        Overloads python power operator (**).
+        """
+
+       
+        return self.__class__(content=arithmetic.Pow(left=other, right=self))
+    
+    #---------------------------------------------------
+    # Array Operators
+    #---------------------------------------------------
+    def array_to_object(self)->Self:
+        """Creates a $arrayToObject expression"""
+
+       
+        return self.__class__(content=array.ArrayToObject(expression=self))
+        
+    
+    def in_(self, right:Self)->Self:
+        """Creates a $in operator"""
+
+       
+        return  self.__class__(content=array.In(left=self, right=right))
+        
+
+    def __contains__(self, right:Self)->Self:
+        """Creates a $in expression"""
+
+        return self.__class__(content=array.In(left=self, right=right))
+
+    def filter(self, query:Self, let:str|None=None, limit:int|None=None)->Self:
+        """"Creates a $filter expression"""
+
+       
+        return  self.__class__(content=array.Filter(
+            expression=self,
+            query=query,
+            let=let,
+            limit=limit
+        ))
+        
+
+    def is_array(self)->Self:
+        """Creates a $isArray expression"""
+
+       
+        return self.__class__(content=array.IsArray(expression=self))
+        
+
+    def max_n(self, limit:int=1)->Self:
+        """Creates a $maxN expression"""
+
+       
+        return self.__class__(content=array.MaxN(expression=self, limit=limit))
+        
+
+    def min_n(self, limit:int=1)->Self:
+        """Creates a $maxN expression"""
+
+       
+        return self.__class__(content=array.MinN(expression=self, limit=limit))
+        
+
+    def sort_array(self, by:dict[str, Literal[1, -1]])->Self:
+        """Creates a $sortArray expression"""
+
+       
+        return self.__class__(content=array.SortArray(expression=self, by=by))
+    
+    
+    #---------------------------------------------------
+    # Boolean (Logical) Operators
     #---------------------------------------------------
     def __and__(self, other:Self)->Self:
         """
@@ -190,127 +411,37 @@ class Expression(BaseModel):
 
        
         return  self.__class__(content=comparison.Cmp(left=self, right=other))
-        
-
-    #---------------------------------------------------
-    # Accumulators (Aggregation) Operators
-    #---------------------------------------------------
-    def average(self)->Self:
-        """Creates an $avg expression"""
-
-       
-        return self.__class__(content=accumulators.Avg(expression=self))
-        
-
-    def count(self)->Self:
-        """Creates a $count expression"""
-
-       
-        return self.__class__(content=accumulators.Count())
-        
-
-    def first(self)->Self:
-        """Creates a $first expression"""
-
-       
-        return  self.__class__(content=accumulators.First(expression=self))
-        
-
-    def last(self)->Self:
-        """Creates a $last expression"""
-
-       
-        return self.__class__(content=accumulators.Last(expression=self))
-        
-
-    def max(self)->Self:
-        """Creates a $max expression"""
-
-       
-        return self.__class__(content=accumulators.Max(expression=self))
-        
-
-    def min(self)->Self:
-        """Creates a $min expression"""
-
-       
-        return self.__class__(content=accumulators.Min(expression=self))
-        
-
-    def push(self)->Self:
-        """Creates a $push expression"""
-
-       
-        return  self.__class__(content=accumulators.Push(expression=self))
-        
-
-    def sum(self)->Self:
-        """Creates a $sum expression"""
-
-        return  self.__class__(content=accumulators.Sum(expression=self))
-        
-
-    #---------------------------------------------------
-    # Array Operators
-    #---------------------------------------------------
-    def array_to_object(self)->Self:
-        """Creates a $arrayToObject expression"""
-
-       
-        return self.__class__(content=array.ArrayToObject(expression=self))
-        
     
-    def in_(self, right:Self)->Self:
-        """Creates a $in operator"""
+    #---------------------------------------------------
+    # Conditional Operators
+    #---------------------------------------------------
+    def cond(self, then_:Self, else_:Self)->Self:
+        """Creates a $cond expression"""
 
        
-        return  self.__class__(content=array.In(left=self, right=right))
-        
+        return self.__class__(content=conditional.Cond(if_=self, then_=then_, else_=else_))
 
-    def __contains__(self, right:Self)->Self:
-        """Creates a $in expression"""
-
-        return self.__class__(content=array.In(left=self, right=right))
-
-    def filter(self, query:Self, let:str|None=None, limit:int|None=None)->Self:
-        """"Creates a $filter expression"""
+    def if_null(self, output:Self)->Self:
+        """Creates a $ifNull expression"""
 
        
-        return  self.__class__(content=array.Filter(
-            expression=self,
-            query=query,
-            let=let,
-            limit=limit
-        ))
-        
-
-    def is_array(self)->Self:
-        """Creates a $isArray expression"""
+        return self.__class__(content=conditional.IfNull(expression=self, output=output))
+    
+    
+    def switch(self, branches:list[Any], default:Self)->Self:
+        """Creates a $switch expression"""
 
        
-        return self.__class__(content=array.IsArray(expression=self))
-        
+        return self.__class__(content=conditional.Switch(expression=self, branches=branches, default=default))
 
-    def max_n(self, limit:int=1)->Self:
-        """Creates a $maxN expression"""
-
-       
-        return self.__class__(content=array.MaxN(expression=self, limit=limit))
-        
-
-    def min_n(self, limit:int=1)->Self:
-        """Creates a $maxN expression"""
+    #---------------------------------------------------
+    # Date Operators
+    #---------------------------------------------------
+    def millisecond(self)->Self:
+        """Creates a $millisecond expression"""
 
        
-        return self.__class__(content=array.MinN(expression=self, limit=limit))
-        
-
-    def sort_array(self, by:dict[str, Literal[1, -1]])->Self:
-        """Creates a $sortArray expression"""
-
-       
-        return self.__class__(content=array.SortArray(expression=self, by=by))
-        
+        return self.__class__(content=date.Millisecond(expression=self))
 
     #---------------------------------------------------
     # Objects Operators
@@ -327,31 +458,36 @@ class Expression(BaseModel):
 
        
         return self.__class__(content=objects.ObjectToArray(expression=self))
+    
+
+    #---------------------------------------------------
+    # String Operators
+    #---------------------------------------------------
+    def concat(self, *args:Self)->Self:
+        """Creates a $concat operator"""
+
+       
+        return self.__class__(content=strings.Concat(expressions=[self, *args]))
+    
+    def date_from_string(self)->Self:
+        """Creates a $dateFromString operator"""
+
+       
+        return self.__class__(content=strings.DateFromString(expression=self))
+    
+    
+    def date_to_string(self)->Self:
+        """Creates a $dateToString operator"""
+
+       
+        return self.__class__(content=strings.DateToString(expression=self))
+
+    #---------------------------------------------------
+    # Type Operators
+    #---------------------------------------------------
+    def type_(self)->Self:
+        """Creates a $type operator"""
+
+       
+        return self.__class__(content=type_.Type_(expression=self))
         
-
-if __name__ == "__main__":
-    result = Expression.field("left") & Expression.field("right")
-    print(result())
-
-# Examples of expression:
-
-{"$sum":1}
-
-{"$type" : "number"}
-
-{ "$avg": { "$multiply": [ "$price", "$quantity" ] } } # awesome example
-
-{ "$avg": "$quantity" }
-
-{ "$first": "$date" }
-
-{ "$mergeObjects": [ { "$arrayElemAt": [ "$fromItems", 0 ] }, "$$ROOT" ] }
-
-{
-    "$map":
-        {
-        "input": "$quizzes",
-        "as": "grade",
-        "in": { "$add": [ "$$grade", 2 ] }
-        }
-}
