3bf0ea69e67ae17c915309b288429d668b9b5c59
Vianney Mixtur <vianney.mixtur@outlook.fr>
2023-09-08 18:00:57 +0200

    Fixed default type for databases

--- monggregate/_run.py
+++ monggregate/_run.py
@@ -1,16 +1,16 @@
-
+from types import NoneType
 try:
     from pymongo.database import Database
     from pymongo.command_cursor import CommandCursor
     PYMONGO = True
 except ImportError:
-    Database = None
+    Database = NoneType
     PYMONGO = False
 try:
     from motor.motor_asyncio import AsyncIOMotorDatabase
     from motor.motor_tornado import MotorDatabase
     MOTOR = True
 except ImportError:
-    AsyncIOMotorDatabase = None
-    MotorDatabase = None
+    AsyncIOMotorDatabase = NoneType
+    MotorDatabase = NoneType
     MOTOR = False
