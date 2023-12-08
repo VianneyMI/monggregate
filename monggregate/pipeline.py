872431b31006ccc23ede549244cec1f331fc9eda
Olivier <ol.legran@gmail.com>
2023-12-08 16:39:48 +0100

    updated docs in add_fields and bucket methods.

--- monggregate/pipeline.py
+++ monggregate/pipeline.py
@@ -247,10 +247,11 @@ class Pipeline(BaseModel): # pylint: disable=too-many-public-methods
         """
         Adds an add_fields stage to the current pipeline.
 
-        Arguments:
-        ---------------------------------
-            - statement, dict :
-            - document, dict : new fields to be added
+        Parameters:
+        -----------
+            statement : dict
+            document : dict
+                new fields to be added
 
         Online MongoDB documentation:
         -----------------------------
@@ -267,48 +268,56 @@ class Pipeline(BaseModel): # pylint: disable=too-many-public-methods
 
     def bucket(self, *, boundaries:list, by:Any=None, group_by:Any=None, default:Any=None, output:dict|None=None)->Self:
         """
-        Adds a bucket stage to the current pipeline.
-        This stage aggregates documents into buckets specified by the boundaries argument.
+        Adds a bucket stage to the current pipeline. This stage aggregates
+        documents into buckets specified by the boundaries argument.
 
-        Arguments:
-        ---------------------------------
-            by : str|list[str]|set[str], field or fields to group the documents
-                                        unless a default is provided, each input document
-                                        must resolve the groupBy field path or expression
-                                        to a value that falls within one of the ranges specified
-                                        by the boundaries
-            boundaries : list, an array of values that specify the boundaries for each bucket.
-                            Each adjacent pair of values acts as the inclusive lower boundary
-                            and the exclusive upper boundary for the bucket.
-                            NOTE : You must specify at least two boundaries.
-            default : Any, Optional. A literal that specifies the _id (group name) of an additional
-                                    bucket that contains all documents whoe groupBy expression result
-                                    does not fall into a bucket specified by the boundaries
+        Parameters
+        ----------
+            by : str|list[str]|set[str]
+                field or fields to group the documents
+                unless a default is provided, each input document
+                must resolve the groupBy field path or expression
+                to a value that falls within one of the ranges specified
+                by the boundaries
+            boundaries : list
+                an array of values that specify the boundaries for each bucket.
+                Each adjacent pair of values acts as the inclusive lower boundary
+                and the exclusive upper boundary for the bucket.
+                NOTE : You must specify at least two boundaries.
+            default : Any, Optional. 
+                A literal that specifies the _id (group name) of an additional
+                bucket that contains all documents whoe groupBy expression result
+                does not fall into a bucket specified by the boundaries. If
+                unspecified, each input document must resolve groupBy expression
+                to a value within one of the bucket ranges. The default value
+                must be less than the lowest boundary or greather than or
+                equal to the highest boundary value. The default value can
+                be of a different type than the entries in boundaries.
+            output : dict | None
+                A document that specifies the fields to include in the output documents in addition to
+                the _id field. To specify the field to include you must use accumulator expressions
+                    >>> {"outputField1" : {"accumulator":"expression1}}
+                        ....
+                        {"outputField2" : {"accumulator":"expression2}}
+                If you do not specify an output document, the operation returns a count field containing
+                the number of documents in each bucket.
 
-                                    If unspecified, each input document must resolve groupBy
-                                    expression to a value within one of the bucket ranges.
+                If you specify and output document, only the fields specified in the document are returned; i.e.
+                the count field is not returned unless it is explicitly included in the output document
 
-                                    The default value must be less than the lowest boundary or greather
-                                    than or equal to the highest boundary value
-
-                                    The default value can be of a different type than the entries in boundaries
-            output : dict | None, A document that specifies the fields to include in the output documents in addition to
-                                the _id field. To specify the field to include you must use accumulator expressions
-                                    >>> {"outputField1" : {"accumulator":"expression1}}
-                                        ....
-                                        {"outputField2" : {"accumulator":"expression2}}
-                                If you do not specify an output document, the operation returns a count field containing
-                                the number of documents in each bucket.
-
-                                If you specify and output document, only the fields specified in the document are returned; i.e.
-                                the count field is not returned unless it is explicitly included in the output document
+        Returns
+        -------
+            self
+                The pipeline object with the bucket stage added.
 
         Online MongoDB documentation:
         ------------------------------
-        Categorizes incoming documents into groups, called buckets, based on a specified expression and bucket boundaries and outputs a document per each bucket. Each output document contains an _id field whose value specifies the inclusive lower bound of the bucket. The
-        output option specifies the fields included in each output document.
-
-        $bucket only produces output documents for buckets that contain at least one input document.                           
+        Categorizes incoming documents into groups, called buckets, based 
+        on a specified expression and bucket boundaries and outputs a document
+        per each bucket. Each output document contains an _id field whose
+        value specifies the inclusive lower bound of the bucket. The output
+        option specifies the fields included in each output document. $bucket
+        only produces output documents for buckets that contain at least one input document.                           
         
         Source :  https://www.mongodb.com/docs/manual/meta/aggregation-quick-reference/
         """
