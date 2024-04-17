9aa59a0759893fd080af2895a87055a18ec6ec49
Vianney Mixtur <59029806+VianneyMI@users.noreply.github.com>
2024-04-17 21:56:20 +0200

    bump version 0.21.0b1 -> 0.21.0 (#114)

--- src/monggregate/__init__.py
+++ src/monggregate/__init__.py
@@ -5,7 +5,7 @@ from monggregate.dollar import S, SS
 from monggregate.pipeline import Pipeline
 
 
-__version__ = "0.21.0b1"
+__version__ = "0.21.0"
 __author__ = "Vianney Mixtur"
 __contact__ = "prenom.nom@outlook.fr"
 __copyright__ = "Copyright © 2022 Vianney Mixtur"
