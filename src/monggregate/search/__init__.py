e2a85ee4d49396897278be02e6a7e43142772989
Vianney Mixtur <59029806+VianneyMI@users.noreply.github.com>
2024-04-21 13:50:30 +0200

    Update src/monggregate/search/__init__.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/monggregate/search/__init__.py
+++ src/monggregate/search/__init__.py
@@ -1,8 +1,36 @@
 """Search Package"""
 
-from monggregate.search.commons import FuzzyOptions, CountOptions, HighlightOptions, HightlightOutput, CountResults
-from monggregate.search.collectors import Facet, Facets, FacetBucket, FacetBuckets, FacetResult, StringFacet, NumericFacet, DateFacet, FacetName
-from monggregate.search.operators import Autocomplete, Compound, Equals, Exists, MoreLikeThis, Range, Regex, Text, Wildcard, AnyOperator, OperatorMap
+from monggregate.search.commons import (
+    FuzzyOptions,
+    CountOptions,
+    HighlightOptions,
+    HightlightOutput,
+    CountResults,
+)
+from monggregate.search.collectors import (
+    Facet,
+    Facets,
+    FacetBucket,
+    FacetBuckets,
+    FacetResult,
+    StringFacet,
+    NumericFacet,
+    DateFacet,
+    FacetName,
+)
+from monggregate.search.operators import (
+    Autocomplete,
+    Compound,
+    Equals,
+    Exists,
+    MoreLikeThis,
+    Range,
+    Regex,
+    Text,
+    Wildcard,
+    AnyOperator,
+    OperatorMap,
+)
 
 __all__ = [
     "FuzzyOptions", 
