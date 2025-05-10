833d38f41ec23fd9a4c5a054b788e8c7b9750386
VianneyMI <vianney.mixtur@outlook.fr>
2025-05-10 22:38:34 +0200

    Add tests bucket auto

---
+++ tests/tests_monggregate/stages/test_bucket_auto.py
@@ -0,0 +1,64 @@
+"""Tests for the BucketAuto stage."""
+
+from monggregate.stages import BucketAuto
+from monggregate.stages.bucket_auto import GranularityEnum
+
+
+class TestBucketAuto:
+    """Tests for the BucketAuto stage."""
+
+    def test_instantiation(self):
+        """Test that the BucketAuto stage can be instantiated."""
+        stage = BucketAuto(
+            group_by="field",
+            buckets=10,
+        )
+        assert isinstance(stage, BucketAuto)
+
+    def test_expression(self):
+        """Test that the expression method returns the correct expression."""
+
+        stage = BucketAuto(group_by="field", buckets=10)
+        # fmt: off
+        assert stage.expression == {
+            "$bucketAuto": {
+                "groupBy": "$field",
+                "buckets": 10,
+                "output": None,
+                "granularity": None,
+                }
+        }
+        # fmt: on
+
+    def test_with_output(self):
+        """Test that the output parameter is validated."""
+
+        stage = BucketAuto(group_by="field", buckets=10, output={"count": {"$sum": 1}})
+        assert stage.expression == {
+            "$bucketAuto": {
+                "groupBy": "$field",
+                "buckets": 10,
+                "output": {"count": {"$sum": 1}},
+                "granularity": None,
+            }
+        }
+
+    def test_with_granularity(self):
+        """Test that the granularity parameter is validated."""
+
+        stage = BucketAuto(group_by="field", buckets=10, granularity="R10")
+
+        # Check that the granularity is stored as an instance of GranularityEnum
+        assert isinstance(stage.granularity, GranularityEnum)
+
+        # fmt: off
+        assert stage.expression == {
+            "$bucketAuto": {
+                "groupBy": "$field",
+                "buckets": 10,
+                "output": None,
+                "granularity": "R10", # Granularity gets serialized correctly
+                                      # When passing to expression
+                }
+        }
+        # fmt: on
