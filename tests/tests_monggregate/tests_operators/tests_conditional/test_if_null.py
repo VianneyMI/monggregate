6e0d3b6d8e26eb4f49d7a6e490a7bdb507ba8c7a
Zegthor91 <zegthorthechazz@gmail.com>
2025-06-06 15:03:10 +0200

    tests_conditional folder passed and completed

--- tests/tests_monggregate/tests_operators/tests_conditional/test_if_null.py
+++ tests/tests_monggregate/tests_operators/tests_conditional/test_if_null.py
@@ -2,6 +2,19 @@
 
 from monggregate.operators.conditional.if_null import IfNull
 
+def test_if_null_expression():
+    # Setup
+    operand = "$optional_field"
+    output = "default_value"
+    expected_expression = {"$ifNull": [operand, output]}
+
+    # Act
+    ifnull_op = IfNull(operand=operand, output=output)
+    result_expression = ifnull_op.expression
+
+    # Assert
+    assert result_expression == expected_expression
+
 
 class TestIfNull:
     """Tests for `IfNull` class."""
