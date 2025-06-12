86cabeabd3819e1d676f5b39f47a49035663c695
Zegthor91 <zegthorthechazz@gmail.com>
2025-06-12 11:04:29 +0200

    2 __init__ functions included

--- tests/tests_monggregate/__init__.py
+++ tests/tests_monggregate/__init__.py
@@ -1,1 +1,7 @@
 """Tests for `monggregate` package."""
+
+from tests.tests_monggregate.test_base import BaseModel, Expression, Singleton, express, isbasemodel
+from tests.tests_monggregate.test_dollar import (AggregationVariableEnum,Dollar,DollarDollar,CLUSTER_TIME,NOW,ROOT,CURRENT,REMOVE,DESCEND,PRUNE,KEEP,CONSTANTS,S,SS,)
+from tests.tests_monggregate.test_fields import FieldName, FieldPath, Variable
+from tests.tests_monggregate.test_pipeline import Pipeline, Match, Project
+from tests.tests_monggregate.test_utils import (to_unique_list,validate_field_path,validate_field_paths,StrEnum,)
--- tests/tests_monggregate/tests_operators/tests_accumulators/__init__.py
+++ tests/tests_monggregate/tests_operators/tests_accumulators/__init__.py
@@ -1,1 +1,11 @@
 """Tests for `monggregate.operators.accumulators` subpackage."""
+
+from tests.tests_monggregate.tests_operators.tests_accumulators.test_accumulator import Accumulator, AccumulatorEnum
+from tests.tests_monggregate.tests_operators.tests_accumulators.test_avg import Average, avg
+from tests.tests_monggregate.tests_operators.tests_accumulators.test_count import Count, count
+from tests.tests_monggregate.tests_operators.tests_accumulators.test_first import First, first
+from tests.tests_monggregate.tests_operators.tests_accumulators.test_last import Last, last
+from tests.tests_monggregate.tests_operators.tests_accumulators.test_max import Max, max
+from tests.tests_monggregate.tests_operators.tests_accumulators.test_min import Min, min
+from tests.tests_monggregate.tests_operators.tests_accumulators.test_push import Push, push
+from tests.tests_monggregate.tests_operators.tests_accumulators.test_sum import Sum, sum
