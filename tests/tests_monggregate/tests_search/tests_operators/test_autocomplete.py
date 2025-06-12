f449848b6e6243c77617b06102324206804d74d4
Zegthor91 <zegthorthechazz@gmail.com>
2025-06-12 14:00:52 +0200

    new __init__ function uploaded

--- tests/tests_monggregate/tests_search/tests_operators/test_autocomplete.py
+++ tests/tests_monggregate/tests_search/tests_operators/test_autocomplete.py
@@ -1,25 +1,54 @@
+from monggregate.search.operators.autocomplete import Autocomplete, TokenOrderEnum
+
 def test_autocomplete_expression():
     # Setup
-    from monggregate.search.operators.autocomplete import Autocomplete, TokenOrderEnum
-
-    autocomplete = Autocomplete(
-        query="hel",
-        path="name"
-    )
-
-    # Act
-    result = autocomplete.expression
-
-    # Assert
-    assert result == {
+    query = "some_query"
+    path = "some_field"
+    expected_expression = {
         "autocomplete": {
-            "query": "hel",
-            "path": "name",
+            "query": query,
+            "path": path,
             "tokenOrder": "any",
             "fuzzy": None,
             "score": None
         }
     }
+
+    # Act
+    autocomplete_op = Autocomplete(query=query, path=path)
+    result_expression = autocomplete_op.expression
+
+
+    # Assert
+    assert result_expression == expected_expression
+
+
+class TestAutocomplete:
+    """Tests for `Autocomplete` class."""
+
+    def test_instantiation(self) -> None:
+        """Test that `Autocomplete` class can be instantiated."""
+        query = "some_query"
+        path = "some_field"
+        auto = Autocomplete(query=query, path=path)
+        assert isinstance(auto, Autocomplete)
+
+    def test_expression_basic(self) -> None:
+        """Test basic expression output of `Autocomplete` class."""
+        query = "some_query"
+        path = "some_field"
+        auto = Autocomplete(query=query, path=path)
+        assert auto.expression == {
+            "autocomplete": {
+                "query": query,
+                "path": path,
+                "tokenOrder": "any",
+                "fuzzy": None,
+                "score": None
+            }
+        }
+
+
 
 
 # TODO
--- tests/tests_monggregate/tests_search/tests_operators/test_clause.py
+++ tests/tests_monggregate/tests_search/tests_operators/test_clause.py
@@ -1,1 +1,62 @@
+from monggregate.search.operators.autocomplete import Autocomplete
+from monggregate.search.operators.equals import Equals
+from monggregate.search.operators.exists import Exists
+from monggregate.search.operators.more_like_this import MoreLikeThis
+from monggregate.search.operators.range import Range
+from monggregate.search.operators.regex import Regex
+from monggregate.search.operators.text import Text
+from monggregate.search.operators.wildcard import Wildcard
+
+from src.monggregate.search.operators.clause import Clause
+
+
+def test_clause_union_type():
+    # Setup
+    autocomplete = Autocomplete(query="test", path="field")
+    equals = Equals(path="field", value="value")
+    exists = Exists(path="field")
+    more_like_this = MoreLikeThis(like="example")
+    range_op = Range(path="field", gte=1, lte=10)
+    regex = Regex(path="field", query="^abc")
+    text = Text(query="something", path="field")
+    wildcard = Wildcard(path="field", query="a*")
+
+    # Act & Assert
+    assert isinstance(autocomplete, Clause)
+    assert isinstance(equals, Clause)
+    assert isinstance(exists, Clause)
+    assert isinstance(more_like_this, Clause)
+    assert isinstance(range_op, Clause)
+    assert isinstance(regex, Clause)
+    assert isinstance(text, Clause)
+    assert isinstance(wildcard, Clause)
+
+class TestClause:
+    """Tests for `Clause` union type."""
+
+    def test_autocomplete_is_clause(self):
+        assert isinstance(Autocomplete(query="x", path="field"), Clause)
+
+    def test_equals_is_clause(self):
+        assert isinstance(Equals(path="field", value="v"), Clause)
+
+    def test_exists_is_clause(self):
+        assert isinstance(Exists(path="field"), Clause)
+
+    def test_more_like_this_is_clause(self):
+        assert isinstance(MoreLikeThis(like="sample"), Clause)
+
+    def test_range_is_clause(self):
+        assert isinstance(Range(path="field", gte=1), Clause)
+
+    def test_regex_is_clause(self):
+        assert isinstance(Regex(path="field", query="abc"), Clause)
+
+    def test_text_is_clause(self):
+        assert isinstance(Text(query="some", path="field"), Clause)
+
+    def test_wildcard_is_clause(self):
+        assert isinstance(Wildcard(path="field", query="*a"), Clause)
+
+
 # TODO
